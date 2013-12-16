5f3cc3b739177e7df6c4c84f2cbaee21f8294a6c
Ian Quick <ian.quick@gmail.com>
2013-12-16 10:58:56 -0500

    matrix4

---
+++ src/matrix4.h
@@ -0,0 +1,15 @@
+#ifndef EV_MATRIX_H_
+#define EV_MATRIX_H_
+
+namespace evil {
+
+class Matrix4
+{
+public:
+		float m[16];
+		void setOrtho(float left, float right, float bottom,
+									float top, float near, float far);
+};
+}
+
+#endif
