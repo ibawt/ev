311114ff675fe18609ddc3b5315e55c4c39e8205
Ian Quick <ian.quick@gmail.com>
2014-02-23 08:21:34 -0500

    cmocka tests with ctest.

---
+++ test/matrix4_test.c
@@ -0,0 +1,68 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include "matrix4.c"
+
+#define abs( x ) (( x < 0 ? -x : x))
+
+#define EPSILON 0.1f
+
+#define assert_float_equals(x,y) assert_true( abs(x - y ) < EPSILON)
+
+static void identity_test(void **state)
+{
+    (void) state;
+
+    ev_matrix4 matrix;
+
+    ev_matrix4_identity(&matrix);
+
+    assert_float_equals(matrix.m[M00], 1.0f);
+    assert_float_equals(matrix.m[M10], 0);
+    assert_float_equals(matrix.m[M20], 0);
+    assert_float_equals(matrix.m[M30], 0);
+
+
+    assert_float_equals(matrix.m[M01], 0);
+    assert_float_equals(matrix.m[M11], 1.0f);
+    assert_float_equals(matrix.m[M21], 0);
+    assert_float_equals(matrix.m[M31], 0);
+
+    assert_float_equals(matrix.m[M02], 0);
+    assert_float_equals(matrix.m[M12], 0);
+    assert_float_equals(matrix.m[M22], 1.0f);
+    assert_float_equals(matrix.m[M32], 0);
+
+    assert_float_equals(matrix.m[M03], 0);
+    assert_float_equals(matrix.m[M13], 0);
+    assert_float_equals(matrix.m[M23], 0);
+    assert_float_equals(matrix.m[M33], 1.0f);
+}
+
+static void set_ortho_test(void **state)
+{
+    const float ortho[] = { 0.002500,0.000000,0.000000,0.000000,
+                      0.000000,-0.003339,0.000000,0.000000,
+                      0.000000,0.000000,-1.000000,0.000000,
+                      -1.000000,1.003339,-0.000000,1.000000 };
+    int i;
+    ev_matrix4 matrix;
+
+    ev_matrix4_set_ortho(&matrix, 0, 800, 600, 1, -1, 1);
+
+
+    for( i = 0 ; i < 16 ; ++i ) {
+        assert_float_equals(matrix.m[i], ortho[i]);
+    }
+}
+
+int main(void)
+{
+    const UnitTest tests[] = {
+        unit_test(identity_test),
+        unit_test(set_ortho_test),
+    };
+
+    return run_tests(tests);
+}
