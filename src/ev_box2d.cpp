ff0e702d700093df8156cbca9720e8308d7f54c2
Ian Quick <ian.quick@gmail.com>
2014-03-17 00:04:07 -0400

    actually this is much cleaner

--- src/ev_box2d.cpp
+++ src/ev_box2d.cpp
@@ -2,32 +2,46 @@
 #include "Box2D/Box2D.h"
 #include "ev_box2d.h"
 
-struct ev_world {
+
+class ev_contact_listener : public b2ContactListener
+{
+public:
+    virtual void BeginContact(b2Contact *b) {
+    }
+    virtual void EndContact(b2Contact *b) {
+    }
 };
 
+
+struct ev_world {
+    ev_world() : world(b2Vec2(0,-9.8)) { }
+    ev_contact_listener listener;
+    b2World world;
+};
 
 
 ev_world* ev_world_create(void)
 {
-    b2World *world = new b2World(b2Vec2(0,-9.8f));
+    ev_world *world;
+
+    world = new (ev_malloc(sizeof(ev_world))) ev_world;
+
     return (ev_world*)world;
 }
 
 void ev_world_destroy(ev_world *world)
 {
     assert( world != NULL );
-    delete (b2World*)world;
+    delete world;
 }
 
 ev_vec2 ev_world_get_gravity(ev_world *world)
 {
-    b2World *b2_world;
     ev_vec2  gravity = { 0.0f, 0.0f };
     assert( world != NULL );
 
     if( world ) {
-        b2_world = (b2World*)world;
-        b2Vec2 b_gravity = b2_world->GetGravity();
+        b2Vec2 b_gravity = world->world.GetGravity();
         gravity.x = b_gravity.x;
         gravity.y = b_gravity.y;
     }
