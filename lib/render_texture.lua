0dc0f1dbe9bed0c0d8b92e9f96ae1263547a430e
Ian Quick <ian.quick@gmail.com>
2014-04-03 10:36:13 -0400

    this works

--- lib/render_texture.lua
+++ lib/render_texture.lua
@@ -14,18 +14,24 @@ local RenderTexture = {}
 RenderTexture.__index = RenderTexture
 
 function RenderTexture.create(width, height)
-   local rtex = {}
-   setmetatable(rtex, RenderTexture)
-   rtex._ev_rtex = C.ev_rtex_create(width, height)
-   return rtex
+	 local rtex = {}
+	 setmetatable(rtex, RenderTexture)
+	 rtex._ev_rtex = C.ev_rtex_create(width, height)
+	 return rtex
 end
 
-function RenderTexture:bind()
-   C.ev_rtex_bind(self._ev_rtex)
+function RenderTexture:update(dt)
 end
 
-function RenderTexture:unbind()
-   C.ev_rtex_unbind(self._ev_tex)
+function RenderTexture:render()
+end
+
+function RenderTexture:lock()
+	 C.ev_rtex_bind(self._ev_rtex)
+end
+
+function RenderTexture:unlock()
+	 C.ev_rtex_unbind(self._ev_tex)
 end
 
 return RenderTexture
